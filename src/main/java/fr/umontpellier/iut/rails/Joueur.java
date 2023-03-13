34d769ee250f0d806ab5640e4aaf881418ecf43d
Starck Soren <soren.starck@etu.umontpellier.fr>
2023-03-13 22:06:08 +0100

    début capture port

--- src/main/java/fr/umontpellier/iut/rails/Joueur.java
+++ src/main/java/fr/umontpellier/iut/rails/Joueur.java
@@ -142,6 +142,11 @@ public class Joueur {
         List<CarteTransport> carteVisible = jeu.getCartesTransportVisibles();
         ArrayList<String> carteVisibleNom = new ArrayList<String>();
 
+        List<Ville> portLibre = jeu.getPortsLibres();
+        ArrayList<String> portLibreNom = new ArrayList<String>();
+
+
+
 
         List<Bouton> boutons = Arrays.asList(
                 new Bouton("Échanger wagon","PIONS WAGON"),
@@ -156,10 +161,16 @@ public class Joueur {
         options.add("PIONS BATEAU");
         options.add("WAGON");
         options.add("BATEAU");
+        options.add("PORT");
         
         for(CarteTransport c : carteVisible){
             options.add(c.getNom());
             carteVisibleNom.add(c.getNom());
+        }
+
+        for(Ville v : portLibre){
+            options.add(v.nom());
+            portLibreNom.add(v.nom());
         }
 
 
@@ -191,8 +202,9 @@ public class Joueur {
                     //TODO (Norman je gère la fougère)
                     aJoue = prendreRoute();
                 }
-                if(choix.equals("Construire un port")){
-                    //TODO
+                if(choix.equals("PORT")){
+                    capturerPort(choix);
+                    aJoue = true;
                 }
             }
         }while(!aJoue);
@@ -456,13 +468,6 @@ public class Joueur {
     }
 
     private void echangerPion(String mode){
-        /*
-        List<Bouton> boutons = Arrays.asList(
-                new Bouton("Échanger un pion Bateau contre un pion Wagon", "Échange un pion Bateau contre un pion Wagon"),
-                new Bouton("Échanger un pion Wagon contre un pion Bateau", "Échange un pion Wagon contre un pion Bateau"),
-                new Bouton("Retour"));
-        List<String> choixWagon = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25");
-        */
         String choix;
         if(mode.equals("PIONS WAGON")){
             int nbWagon = 0;
@@ -519,14 +524,36 @@ public class Joueur {
                 log(String.format("L'échange est impossible, pas assez de Pions Bateau\n à échanger.",toLog()));
             }
         }
+    }
 
 
-        /*
+    private void capturerPort(String port){
+        //capturer le port qui est donné en paramètre et qui est dans la liste des ports, si il est déjà dans la liste c'est qu'il est capturable
+        List<Ville> portLibre = jeu.getPortsLibres();
+        if(routeRelieAPort(port) != null){
+            if(routeRelieAPort(port).getLongueur() <= nombreCarteTransport(TypeCarteTransport.BATEAU)){
+                this.ports.add(portLibre.get(ports.indexOf(port)));
+                log(String.format("Vous venez de capturer le port de "+port+".",toLog()));
+            }
+            else{
+                log(String.format("Vous n'avez pas assez de cartes bateau pour capturer le port de "+port+".",toLog()));
+            }
+        }
+        else{
+            log(String.format("Vous n'avez pas de route maritime qui relie le port de "+port+".",toLog()));
+        }
 
-        do{
 
-        }while(mode!="Retour");
-         */
+    }
+
+    private Route routeRelieAPort(String port){
+        //vérifie si le joueur possède une route terrestre ou maritime qui est relié au port
+        for(Route r : this.routes){
+            if(r.getVille1().equals(port) || r.getVille2().equals(port)){
+                return r;
+            }
+        }
+        return null;
     }
 
 
